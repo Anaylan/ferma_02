243fc79566b687eed907eabf86348dda322778bf
Anaylan <sasha.skobelev90@gmail.com>
2023-04-05 10:36:49 +0600

    fix validation schema

--- src/lib/validSchemas.ts
+++ src/lib/validSchemas.ts
@@ -1,9 +1,7 @@
 import * as Yup from "yup";
 
 export const mainPageValidationSchema = Yup.object().shape({
-	who: Yup.string()
-		.min(2, "Ваше имя слишком короткое")
-		.required("Введите ваше имя!"),
+	who: Yup.string().trim().required("Введите ваше имя!"),
 	phone: Yup.string()
 		.min(16, "Номер телефона слишком короткий")
 		.max(16, "Номер телефона слишком длинный")
